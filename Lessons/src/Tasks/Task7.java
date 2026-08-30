2279e763134676a8b82ed9fb5e0ce52084cacd8b
Aitan0x0 <aytanqurbanova00@gmail.com>
2026-08-30 19:49:56 +0400

    Factorial

--- Lessons/src/Tasks/Task7.java
+++ Lessons/src/Tasks/Task7.java
@@ -1,4 +1,23 @@
+//Faktorialin tapilmasi
+
 package Tasks;
 
+import java.util.Scanner;
+
 public class Task7 {
+    public static void main(String[] args) {
+        System.out.print("Eded daxil edin: ");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.printf("%d! = %d%n", num, calculateFactorial(num));
+        scanner.close();
+    }
+
+    public static long calculateFactorial(int num) {
+        long facNum = 1;
+        for (int i = 1; i <= num; i++) {
+            facNum *= i;
+        }
+        return facNum;
+    }
 }
