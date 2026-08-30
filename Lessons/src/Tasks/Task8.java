9ca83e71a342e8f1691775ea7ff3934d2b8b7963
Aitan0x0 <aytanqurbanova00@gmail.com>
2026-08-30 19:50:41 +0400

    String polindromun yoxlanmasi

--- Lessons/src/Tasks/Task8.java
+++ Lessons/src/Tasks/Task8.java
@@ -1,4 +1,27 @@
+//Polindromun yoxlanilmasi (string ucun)
+
 package Tasks;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Task8 {
+    public static void main(String[] args) {
+        System.out.print("Her hansi bir soz daxil edin: ");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.printf(str + (isPolindromeString(str) ? " Polindromdur" : " Polindrom deyil"));
+        scanner.close();
+    }
+
+    public static boolean isPolindromeString(String str) {
+
+        for (int i = 0, j = str.length() - 1; i < str.length(); j--, i++) {
+            if (str.toLowerCase().charAt(i) != str.toLowerCase().charAt(j)) {
+                return false;
+            }
+        }
+        return true;
+
+    }
 }
